e31574d209e4a93aec8ef50f0f0c0796152b712a
Zaldana <alexandrazaldana@gmail.com>
2019-02-03 14:27:44 -0800

    final edits

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,7 @@
+$(document).ready(function(){
+
+// Initialize Firebase //
+
 var config = {
     apiKey: "AIzaSyCrPOq1mLowcEICph8Q4MiBV2pgDBV7dAM",
     authDomain: "train-schedule-3752d.firebaseapp.com",
@@ -8,8 +12,6 @@ var config = {
   };
   firebase.initializeApp(config);
 
-  $(document).ready(function(){
-
 
   var database = firebase.database();
 
@@ -18,26 +20,18 @@ var config = {
   var firstTrain = "";
   var frequency = 0;
 
-  var time = null,
-  date = null;
+  // Create clock //
 
-  var update = function () {
-    date = moment(new Date())
-    time.html(date.format('h:mm:ss a'));
-};
-
-$(document).ready(function(){
-    time = $('#clock')
-    update();
-    setInterval(update, 1000);
-});
+  setInterval(function(){
+    $('#clock').html(moment().format('hh:mm:ss A'))
+  }, 1000);
 
   
 $("#add-train").on("click", function(event) {
-    // prevent page from refreshing when form tries to submit itself
+    // prevent page from refreshing when form tries to submit itself //
     event.preventDefault();
 
-    // Capture user inputs and store them into variables
+    // Capture user inputs and store them into variables //
     trainName = $("#train-name").val().trim();
     destination = $("#destination").val().trim();
     firstTrain = $("#first-train").val().trim();
@@ -49,8 +43,6 @@ $("#add-train").on("click", function(event) {
     var minutesAway = Number(frequency) - minutesFromLastTrain;
     currentTime.add(minutesAway, "minutes");
     var nextArrival = currentTime.format("hh:mm");
-
-   
     
     console.log("this is the difference", difference);
     console.log("minutes away", minutesAway);
@@ -60,6 +52,7 @@ $("#add-train").on("click", function(event) {
     $("#destination").val('');
     $("#first-train").val('');
     $("#frequency").val('');
+    
 
     database.ref().push({
         trainName:trainName,
@@ -68,8 +61,9 @@ $("#add-train").on("click", function(event) {
         frequency:frequency,
         nextArrival:nextArrival,
         minutesAway:minutesAway,
-
     })
+    
+    return false;
 })
 
 database.ref().on("child_added", function(snap){
@@ -90,8 +84,5 @@ database.ref().on("child_added", function(snap){
     console.log(save.destination);
     console.log(save.firstTrain);
     console.log(save.frequency);
+    })
 })
-
-
-
-  })
